5986982b842b884afccac4927698487b501333d6
jdboy <645633369@qq.com>
2019-11-27 18:14:54 +0800

    将highlightjs代码展示插件安装后再次提交

--- src/views/CourseCatalogue.vue
+++ src/views/CourseCatalogue.vue
@@ -71,6 +71,14 @@ export default {
     return {
       msg: '课程目录'
     }
+  },
+  mounted () {
+    axios
+      .get('https://www.runoob.com/try/ajax/json_demo.json')
+      .then(response => (this.info = response))
+      .catch(function (error) { // 请求失败处理
+        console.log(error)
+      })
   }
 }
 </script>
